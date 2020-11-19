adf0ec62d49e4724980b43a695947afd455f40d0
liendmch <mon823@naver.com>
2020-11-19 15:59:51 +0900

    [BE] feat : Sequelize 모델 App 연결 및 연결 확인
    - 2개의 데이터베이스를 위해 2개를 만들어서 싱크함

--- WEB/BE/app.js
+++ WEB/BE/app.js
@@ -4,7 +4,8 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
-const { sequelize } = require('./models/sequelize');
+const sequelizeWEB = require('./models/sequelizeIOS').sequelize;
+const sequelizeIOS = require('./models/sequelizeWEB').sequelize;
 const debug = require('debug')('server:server');
 const http = require('http');
 const port = normalizePort(process.env.PORT || '3000');
@@ -14,7 +15,8 @@ const userRouter = require('./routes/user');
 const app = express();
 app.set('port', port);
 
-sequelize.sync();
+sequelizeWEB.sync();
+sequelizeIOS.sync();
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -25,16 +27,16 @@ app.set('view engine', 'html');
 app.use('/user', userRouter);
 
 app.use(function (req, res, next) {
-    next(createError(404));
+  next(createError(404));
 });
 
 // error handler
 app.use(function (err, req, res, next) {
-    res.locals.message = err.message;
-    res.locals.error = req.app.get('env') === 'development' ? err : {};
-    console.error(err.message);
-    res.status(err.status || 500);
-    res.json({ result: err.message });
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  console.error(err.message);
+  res.status(err.status || 500);
+  res.json({ result: err.message });
 });
 
 const server = http.createServer(app);
@@ -43,45 +45,44 @@ server.on('error', onError);
 server.on('listening', onListening);
 
 function normalizePort(val) {
-    const port = parseInt(val, 10);
+  const port = parseInt(val, 10);
 
-    if (isNaN(port)) {
-        return val;
-    }
+  if (isNaN(port)) {
+    return val;
+  }
 
-    if (port >= 0) {
-        return port;
-    }
+  if (port >= 0) {
+    return port;
+  }
 
-    return false;
+  return false;
 }
 
 function onError(error) {
-    if (error.syscall !== 'listen') {
-        throw error;
-    }
+  if (error.syscall !== 'listen') {
+    throw error;
+  }
 
-    const bind = typeof port === 'string' ? 'Pipe ' + port : 'Port ' + port;
+  const bind = typeof port === 'string' ? 'Pipe ' + port : 'Port ' + port;
 
-    switch (error.code) {
-        case 'EACCES':
-            console.error(bind + ' requires elevated privileges');
-            process.exit(1);
-            break;
-        case 'EADDRINUSE':
-            console.error(bind + ' is already in use');
-            process.exit(1);
-            break;
-        default:
-            throw error;
-    }
+  switch (error.code) {
+    case 'EACCES':
+      console.error(bind + ' requires elevated privileges');
+      process.exit(1);
+      break;
+    case 'EADDRINUSE':
+      console.error(bind + ' is already in use');
+      process.exit(1);
+      break;
+    default:
+      throw error;
+  }
 }
 
 function onListening() {
-    const addr = server.address();
-    const bind =
-        typeof addr === 'string' ? 'pipe ' + addr : 'port ' + addr.port;
-    debug('Listening on ' + bind);
+  const addr = server.address();
+  const bind = typeof addr === 'string' ? 'pipe ' + addr : 'port ' + addr.port;
+  debug('Listening on ' + bind);
 }
 
 module.exports = app;
