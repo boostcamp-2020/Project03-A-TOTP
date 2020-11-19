778d80b00c3638db3eac399bf92d7a591513e06a
liendmch <mon823@naver.com>
2020-11-19 16:50:32 +0900

    [BE] add : Sequelize seeder 구현
    - 더미 데이터 연동 확인
    - 데이터 베이스 2개를 전부 진행하는 방법은 모르겠음
    - 우선 WEB만 사용 가능 iOS  필요시 코드 변경 필요

--- WEB/BE/config/config.js
+++ WEB/BE/config/config.js
@@ -1,6 +1,27 @@
 const dotenv = require('dotenv').config();
 
 module.exports = {
+  development: {
+    username: process.env.DBUSER,
+    password: process.env.DBPASS,
+    database: process.env.DBDATABASEWEB,
+    host: process.env.DBHOST,
+    dialect: 'mysql',
+  },
+  test: {
+    username: process.env.DBUSER,
+    password: process.env.DBPASS,
+    database: process.env.DBDATABASEWEB,
+    host: process.env.DBHOST,
+    dialect: 'mysql',
+  },
+  production: {
+    username: process.env.DBUSER,
+    password: process.env.DBPASS,
+    database: process.env.DBDATABASEWEB,
+    host: process.env.DBHOST,
+    dialect: 'mysql',
+  },
   WEB: {
     development: {
       username: process.env.DBUSER,
