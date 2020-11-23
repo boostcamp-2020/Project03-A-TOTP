883ee33039b1ea48ce72750d1d1a686bc44e4f32
dogyeong <shroad1802@naver.com>
2020-11-23 17:18:22 +0900

    [FE] add: react-router
    - react-router-dom, @types/react-router-dom 설치
    - App 컴포넌트에 BrowserRouter 추가

--- WEB/FE/src/components/App.tsx
+++ WEB/FE/src/components/App.tsx
@@ -1,7 +1,15 @@
 import React from 'react';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
 const App = (): JSX.Element => {
-  return <>Hello, TOTP!!!</>;
+  return (
+    <BrowserRouter>
+      <Switch>
+        {/** @TODO component 추가 */}
+        <Route exact path='/' />
+      </Switch>
+    </BrowserRouter>
+  );
 };
 
 export { App };
