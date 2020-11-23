ce431917581e81febae17f7c4643f90c1f147b24
dogyeong <shroad1802@naver.com>
2020-11-23 18:09:11 +0900

    [FE] add: PrivateRoute 추가
    - 인증 여부에 따라 라우팅을 할 수 있는 PrivateRoute 추가

--- WEB/FE/src/components/App.tsx
+++ WEB/FE/src/components/App.tsx
@@ -1,14 +1,19 @@
 import React from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import { PrivateRoute } from './route/PrivateRoute';
 
 interface AppProps {}
+
+const Hello = () => <>Hello</>;
+const No = () => <>No</>;
 
 const App: React.FC<AppProps> = () => {
   return (
     <BrowserRouter>
       <Switch>
         {/** @TODO component 추가 */}
-        <Route exact path='/' />
+        <Route exact path='/' component={Hello} />
+        <PrivateRoute path='/user' component={No} />
       </Switch>
     </BrowserRouter>
   );
