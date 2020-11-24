51dc3f948d76f06b25a723a4c43b1acd5624cd8d
JinGu <xortm854@naver.com>
2020-11-24 15:32:30 +0900

    feature : auth router 구현
    사용자에게 요청을 받으면 controller에게 넘겨주는 router를
    구현하였습니다.

--- WEB/BE/routes/auth/index.js
+++ WEB/BE/routes/auth/index.js
@@ -2,6 +2,6 @@ const express = require('express');
 const router = express.Router();
 const authController = require('@controllers/auth');
 
-router.post('/check',authController.checkId);
+router.post('/check', authController.checkId);
 
 module.exports = router;
