419b3b2982237fd962db43afed9d48d31753d129
eojin <eozinee@gmail.com>
2020-11-24 14:10:09 +0900

    [iOS] init: 프로젝트 생성

--- WEB/FE/src/components/App.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const App = (): JSX.Element => {
-  return <>Hello, TOTP!!!</>;
-};
-
-export { App };
---
+++ iOS/DaDaIkSeon/DaDaIkSeon/DaDaIkSeonApp.swift
@@ -0,0 +1,17 @@
+//
+//  DaDaIkSeonApp.swift
+//  DaDaIkSeon
+//
+//  Created by 양어진 on 2020/11/24.
+//
+
+import SwiftUI
+
+@main
+struct DaDaIkSeonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
