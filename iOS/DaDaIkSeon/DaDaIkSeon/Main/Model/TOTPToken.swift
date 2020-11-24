e6ad9cb5812606f51a3e8cda65553b12a0200773
jjm159 <wjdwoaud15@gmail.com>
2020-11-24 22:44:27 +0900

    [iOS] feat: TokenModel 추가
    - Model 그룹, TokenModel 파일 추가
    - TOTPToken struct 추가

---
+++ iOS/DaDaIkSeon/DaDaIkSeon/Main/Model/TOTPToken.swift
@@ -0,0 +1,17 @@
+//
+//  TOTPToken.swift
+//  DaDaIkSeon
+//
+//  Created by 정재명 on 2020/11/24.
+//
+
+import Foundation
+
+struct TOTPToken {
+    var key: String?
+    var tokenName: String?
+    var color: String?
+    var icon: String?
+}
+
+// 필요하면 추가
