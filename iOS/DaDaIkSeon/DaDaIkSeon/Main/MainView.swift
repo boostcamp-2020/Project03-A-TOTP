bdd0bacd611f5df27b2424a5a441de7b9da0d69f
jjm159 <wjdwoaud15@gmail.com>
2020-11-24 18:21:35 +0900

    [iOS] feat: MainCellView 레이아웃 구현
    - 아직 할 게 태산..!

--- iOS/DaDaIkSeon/DaDaIkSeon/Main/MainView.swift
+++ iOS/DaDaIkSeon/DaDaIkSeon/Main/MainView.swift
@@ -15,7 +15,8 @@ struct MainView: View {
     ]
     
     var body: some View {
-        VStack {
+        VStack(spacing: 12) {
+            MainCellView()
             LazyVGrid(columns: columns,
                       spacing: 12) {
                 TokenCellView()
@@ -24,8 +25,63 @@ struct MainView: View {
                 TokenCellView()
                 TokenCellView()
             }
+            Spacer()
         }
         .padding(16)
+    }
+}
+
+struct MainCellView: View {
+    var body: some View {
+        ZStack {
+            
+            // 1
+            VStack {
+                HStack {
+                    Image(systemName: "circle")
+                    Spacer()
+                    Image(systemName: "circle")
+                }
+                .padding(.horizontal, 12)
+                .frame(height: 50, alignment: .center) // 크기를 자동으로 하는 방법 고민
+                Spacer()
+                HStack {
+                    Spacer()
+                    Image(systemName: "circle")
+                }
+                .padding(.horizontal, 12)
+                .frame(height: 50, alignment: .center)
+            }
+            .background(Color.green)
+            .cornerRadius(15)
+            
+            // 2
+            Circle()
+                .strokeBorder(Color.white, lineWidth: 15)
+                .frame(height: 176)
+            
+            // 3
+            VStack(spacing: 10) {
+                Spacer()
+                    .frame(height: 30)
+                
+                Text("Token의 이름\n 2줄까지 가능합니다.")
+                    .lineLimit(2)
+                    .multilineTextAlignment(.center)
+                    .frame(
+                        width: 150,
+                        alignment: .center)
+                    
+                Text("333 444")
+                
+                Text("15")
+                    .padding(.top)
+                
+                Spacer()
+                    .frame(height: 30)
+            }
+        }
+        .frame(height: 200)
     }
 }
 
