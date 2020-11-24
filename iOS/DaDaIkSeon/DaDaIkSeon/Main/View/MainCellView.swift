2ed80051fb9f3daa06142740c91577f6768c2491
jjm159 <wjdwoaud15@gmail.com>
2020-11-24 18:37:30 +0900

    [iOS] chore: 파일 분리

---
+++ iOS/DaDaIkSeon/DaDaIkSeon/Main/View/MainCellView.swift
@@ -0,0 +1,20 @@
+//
+//  MainCellView.swift
+//  DaDaIkSeon
+//
+//  Created by 정재명 on 2020/11/24.
+//
+
+import SwiftUI
+
+struct MainCellView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct MainCellView_Previews: PreviewProvider {
+    static var previews: some View {
+        MainCellView()
+    }
+}
---
+++ iOS/DaDaIkSeon/DaDaIkSeon/Main/View/TokenCellView.swift
@@ -0,0 +1,20 @@
+//
+//  TokenCellView.swift
+//  DaDaIkSeon
+//
+//  Created by 정재명 on 2020/11/24.
+//
+
+import SwiftUI
+
+struct TokenCellView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct TokenCellView_Previews: PreviewProvider {
+    static var previews: some View {
+        TokenCellView()
+    }
+}
