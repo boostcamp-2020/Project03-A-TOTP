b61721947306e1e50095c8478d65478f66348a4d
JinGu <xortm854@naver.com>
2020-11-25 14:05:33 +0900

    feature : 회원가입 API 구현 - #13, #16
    회원가입 API 요청이 들어왔을 때 UserInfo로 User 테이블에 값을 입력하고,
    결과값으로 나온 인덱스를 가지고 Auth 테이블에 사용자의 아이디, 비밀번호,
    비밀키, 상태를 넣어줬습니다. 시크릿키는 speakeasy 를 사용하여 생성하였고
    qrcode는 qrcode API를 사용하여 생성하였습니다.

---
+++ WEB/BE/controllers/user/index.js
@@ -0,0 +1,78 @@
+const speakeasy = require('speakeasy');
+const QRCode = require('qrcode');
+const authService = require('@services/auth');
+const userService = require('@services/user');
+const { encrypWithAES256 } = require('@utils/crypto');
+const { encryptedPassword } = require('@utils/bcrypt');
+
+function userController() {}
+
+userController.signUp = async (req, res, next) => {
+  let userInfo = {
+    email: req.body.email,
+    name: req.body.name,
+    birth: req.body.birth,
+    phone: req.body.phone,
+  };
+  const { id, password } = req.body;
+  try {
+    userInfo = encrypUserInfo({ userInfo });
+    const secretKey = makeSecretKey();
+    const qrcode = await makeQRCode({ secretKey });
+    const encryptPassword = await encryptedPassword(password);
+    const insertResult = await userService.insert({ userInfo, next });
+    const result = await authService.insert({
+      idx: insertResult.dataValues.idx,
+      id,
+      password: encryptPassword,
+      state: '0',
+      secretKey: secretKey.base32,
+      next,
+    });
+    res.json({ result, qrcode });
+  } catch (e) {
+    next(e);
+  }
+};
+
+userController.check = async (req, res, next) => {
+  let { id, email } = req.body;
+  id = id ? encrypWithAES256({ Text: id }) : undefined;
+  email = email ? encrypWithAES256({ Text: email }) : undefined;
+  try {
+    const result = id ? await authService.check({ id, next }) : await userService.check({ email, next });
+    res.json({ result });
+  } catch (e) {
+    next(e);
+  }
+};
+
+const encrypUserInfo = ({ userInfo }) => {
+  Object.keys(userInfo).forEach((key) => {
+    userInfo[key] = encrypWithAES256({ Text: userInfo[key] });
+  });
+  return userInfo;
+};
+
+const makeSecretKey = () => {
+  const secretKey = speakeasy.generateSecret({
+    length: 20,
+    name: process.env.SECRETKEYNAME,
+    algorithm: process.env.SECRETKEYALGORITHM,
+  });
+  return secretKey;
+};
+
+const makeQRCode = async ({ secretKey }) => {
+  const url = speakeasy.otpauthURL({
+    secret: secretKey.ascii,
+    issuer: 'TOTP',
+    label: process.env.SECRETKEYLABEL,
+    algorithm: process.env.SECRETKEYALGORITHM,
+    period: 60,
+  });
+  const qrcode = (await QRCode.toDataURL(url)).split('base64,')[1];
+  return qrcode;
+};
+
+module.exports = userController;
