e7beb70f7b4f60d1a8b96c92671a7abc4d5caf28
eojin <eozinee@gmail.com>
2020-11-25 21:50:49 +0900

    [iOS] feat: MainView UI 추가
    - 선택, 마이페이지 버튼 추가

--- iOS/DaDaIkSeon/DaDaIkSeon/Main/View/MainView.swift
+++ iOS/DaDaIkSeon/DaDaIkSeon/Main/View/MainView.swift
@@ -9,6 +9,7 @@ import SwiftUI
 
 struct MainView: View {
     @ObservedObject private var viewModel = TokenListViewModel()
+    @State var searchText: String = ""
     
     init() {
         viewModel.fetchTokens()
@@ -16,12 +17,44 @@ struct MainView: View {
     
     var body: some View {
         VStack(spacing: 12) {
+            HStack {
+                
+                Button(action: {
+                    // 내 정보 페이지 띄우는 곳
+                }, label: {
+                    Image(systemName: "person.crop.circle")
+                        .resizable()
+                        .frame(width: 25, height: 25)
+                        .foregroundColor(.black)
+                        .padding(.leading, 4)
+                    
+                })
+                
+                Spacer()
+                
+                Button(action: {
+                    // Edit Mode로 전환
+                }, label: {
+                    Text("선택")
+                        .padding(.trailing, 4)
+                        .foregroundColor(.black)
+                })
+                  
+            }
+            // 나아아중에 searchbar view 로 바꿔야함.
+            TextField("Search", text: $searchText)
+                .padding(7)
+                .background(Color(.systemGray6))
+                .cornerRadius(8)
+            
             MainCellView()
             ScrollView {
                 TokenListView(tokens: $viewModel.tokens)
             }
-        }.padding([.top, .leading, .trailing])
+        }
+        .padding([.top, .leading, .trailing])
     }
+    
 }
 
 struct MainView_Previews: PreviewProvider {
