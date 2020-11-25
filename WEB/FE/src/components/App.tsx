783258ee4be8a78a0c9be214639a1d59539a764f
dogyeong <shroad1802@naver.com>
2020-11-25 18:08:22 +0900

    [FE] add: recaptcha 추가
    - reCaptcha v3 추가
      - react-google-recaptcha-ve 라이브러리 설치
      - google reCaptcha 등록, 키 발급
    - types 폴더 추가
      - @type 없는 라이브러리의 경우 타입 선언 가능
      - tsconfig.json 수정

--- WEB/FE/src/components/App.tsx
+++ WEB/FE/src/components/App.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import { GoogleReCaptchaProvider } from 'react-google-recaptcha-v3';
 import { PrivateRoute } from './route/PrivateRoute';
 
 interface AppProps {}
@@ -9,13 +10,15 @@ const No = () => <>No</>;
 
 const App: React.FC<AppProps> = () => {
   return (
-    <BrowserRouter>
-      <Switch>
-        {/** @TODO component 추가 */}
-        <Route exact path='/' component={Hello} />
-        <PrivateRoute path='/user' component={No} />
-      </Switch>
-    </BrowserRouter>
+    <GoogleReCaptchaProvider reCaptchaKey='6LcsnewZAAAAAOHzbLjkR4CvWRBNdibrcmtHd8SD' language='ko'>
+      <BrowserRouter>
+        <Switch>
+          {/** @TODO component 추가 */}
+          <Route exact path='/' component={Hello} />
+          <PrivateRoute path='/user' component={No} />
+        </Switch>
+      </BrowserRouter>
+    </GoogleReCaptchaProvider>
   );
 };
 
