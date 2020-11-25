9a3b17208723f49366cee21409acc0b74915e937
JinGu <xortm854@naver.com>
2020-11-25 09:56:37 +0900

    fix : Check API를 User로 이동
    사전에 계획했을 때 Check API를 User로 구현하기로 약속했기 때문에 User로
    코드를 이동하였습니다.

--- WEB/BE/routes/auth/index.js
+++ WEB/BE/routes/auth/index.js
@@ -2,6 +2,4 @@ const express = require('express');
 const router = express.Router();
 const authController = require('@controllers/auth');
 
-router.post('/check', authController.check);
-
 module.exports = router;
