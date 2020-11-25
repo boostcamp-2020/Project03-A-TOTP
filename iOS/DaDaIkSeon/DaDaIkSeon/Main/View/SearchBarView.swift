75762f0c8377fa5480132f477fcd12117f591d95
eojin <eozinee@gmail.com>
2020-11-25 23:35:19 +0900

    [iOS] feat: SearchBarView UI 추가
    - SearchBarView 에 취소 애니매이션 추가

--- iOS/DaDaIkSeon/DaDaIkSeon/Main/View/SearchBarView.swift
+++ iOS/DaDaIkSeon/DaDaIkSeon/Main/View/SearchBarView.swift
@@ -8,13 +8,66 @@
 import SwiftUI
 
 struct SearchBarView: View {
+    
+    @Binding var text: String
+    @State private var isEditing = false
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        HStack {
+            TextField("검색", text: $text)
+                .padding(7)
+                .padding(.horizontal, 25)
+                .background(Color(.systemGray6))
+                .cornerRadius(8)
+                .overlay(
+                    HStack {
+                        Image(systemName: "magnifyingglass")
+                            .foregroundColor(.gray)
+                            .frame(minWidth: 0, maxWidth: .infinity, alignment: .leading)
+                            .padding(.leading, 8)
+                        
+                        if isEditing {
+                            // X버튼
+                            Button(action: {
+                                text = ""
+                            }, label: {
+                                Image(systemName: "multiply.circle.fill")
+                                    .foregroundColor(.gray)
+                                    .padding(.trailing, 8)
+                            })
+                        }
+                    }
+                )
+                .padding(.horizontal, 8)
+                .onTapGesture {
+                    isEditing.toggle()
+                }
+                .animation(.default)
+            
+            if isEditing {
+                // 취소 버튼
+                Button(action: {
+                    isEditing.toggle()
+                    text = ""
+                    // 키보드 닫기
+                    UIApplication.shared.sendAction(#selector(UIResponder.resignFirstResponder),
+                                                    to: nil,
+                                                    from: nil,
+                                                    for: nil)
+                }, label: {
+                    Text("취소")
+                        .foregroundColor(.black)
+                })
+                .padding(.trailing, 10)
+                .transition(.move(edge: .trailing))
+                .animation(.default)
+            }
+        }
     }
 }
 
 struct SearchBarView_Previews: PreviewProvider {
     static var previews: some View {
-        SearchBarView()
+        SearchBarView(text: .constant(""))
     }
 }
