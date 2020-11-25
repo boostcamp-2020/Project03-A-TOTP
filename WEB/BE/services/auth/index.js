a6cd2e7e3324b5af1dd710aa671a5b053f82419a
JinGu <xortm854@naver.com>
2020-11-25 13:30:33 +0900

    fix : Try,Catch Error 처리
    Service안에서 Error를 잡기위해서 인자로 next를 함께 넘겨받아 에러발생시
    next(e)를 호출하여 에러를 처리하였습니다.

--- WEB/BE/services/auth/index.js
+++ WEB/BE/services/auth/index.js
@@ -1,13 +1,26 @@
 const model = require('@models/sequelizeWEB.js');
-
+const authsModel = model.auths;
 function authService() {}
 
-authService.check = async ({ id }) => {
-  const authsModel = model.auths;
+authService.check = async ({ id, next }) => {
   const query = {};
   query.where = { id };
-  const result = await authsModel.findAll(query);
-  return result.length === 0;
+  try {
+    const result = await authsModel.findAll(query);
+    return result.length === 0;
+  } catch (e) {
+    next(e);
+  }
+};
+
+authService.insert = async ({ idx, id, password, state, secretKey, next }) => {
+  const query = { id, password, state, secret_key: secretKey, user_idx: idx };
+  try {
+    const result = await authsModel.create(query);
+    return result;
+  } catch (e) {
+    next(e);
+  }
 };
 
 module.exports = authService;
