0f2da072f1f5b6c6ebfc3b8d0729fbf650ba762d
dogyeong <shroad1802@naver.com>
2020-11-25 21:41:03 +0900

    [BE] refactor: 주석 제거
    - csrf.js 주석 제거

--- WEB/BE/middlewares/csrf.js
+++ WEB/BE/middlewares/csrf.js
@@ -2,14 +2,11 @@ const createError = require('http-errors');
 
 const csrf = {
   checkHeader(req, res, next) {
-    // custom header
     const csrfHeader = req.headers['X-CSRF'] || req.headers['x-csrf'];
 
-    // origin header
     const originHeader = req.headers.origin;
     const isValidOrigin = originHeader && /^(http|https):\/\/dadaikseon\.com/.test(originHeader);
 
-    // referrer header
     const referrerHeader = req.headers.referer;
     const isValidReferrer = referrerHeader && /^(http|https):\/\/dadaikseon\.com/.test(originHeader);
 
