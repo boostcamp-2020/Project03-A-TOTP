6557cbb38a6cd46c60f5bc0671a376be9e7c43c8
JinGu <xortm854@naver.com>
2020-11-25 14:01:12 +0900

    feature : User Model Insert 메소드 구현- #13
    회원가입 API를 요청했을 때 Body로 들어온 name, email, birth, phone의
    정보를 Table에 insert해주는 Service 를 구현하였습니다.

--- WEB/BE/services/user/index.js
+++ WEB/BE/services/user/index.js
@@ -2,12 +2,27 @@ const model = require('@models/sequelizeWEB.js');
 
 function userService() {}
 
-userService.check = async ({ email }) => {
+userService.check = async ({ email, next }) => {
   const usersModel = model.users;
   const query = {};
   query.where = { email };
-  const result = await usersModel.findAll(query);
-  return result.length === 0;
+  try {
+    const result = await usersModel.findAll(query);
+    return result.length === 0;
+  } catch (e) {
+    next(e);
+  }
+};
+
+userService.insert = async ({ userInfo, next }) => {
+  const usersModel = model.users;
+  const query = { email: userInfo.email, name: userInfo.name, birth: userInfo.birth, phone: userInfo.phone };
+  try {
+    const result = await usersModel.create(query);
+    return result;
+  } catch (e) {
+    next(e);
+  }
 };
 
 module.exports = userService;
