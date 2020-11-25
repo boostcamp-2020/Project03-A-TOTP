a3efc85458a34bec4d2f21a2b32e7ffc139a3b2e
dogyeong <shroad1802@naver.com>
2020-11-25 23:37:33 +0900

    [FE] feat: ThemeProvider 추가
    - variable.ts를 theme.ts로 바꾸고 theme 객체를 export하도록 수정
    - App.tsx에 styled-components의 ThemeProvider 추가

--- WEB/FE/src/components/App.tsx
+++ WEB/FE/src/components/App.tsx
@@ -1,7 +1,9 @@
 import React from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import { GoogleReCaptchaProvider } from 'react-google-recaptcha-v3';
+import { ThemeProvider } from 'styled-components';
 import { PrivateRoute } from './route/PrivateRoute';
+import { theme } from '../style/theme';
 
 interface AppProps {}
 
@@ -11,13 +13,15 @@ const No = () => <>No</>;
 const App: React.FC<AppProps> = () => {
   return (
     <GoogleReCaptchaProvider reCaptchaKey='6LcsnewZAAAAAOHzbLjkR4CvWRBNdibrcmtHd8SD' language='ko'>
-      <BrowserRouter>
-        <Switch>
-          {/** @TODO component 추가 */}
-          <Route exact path='/' component={Hello} />
-          <PrivateRoute path='/user' component={No} />
-        </Switch>
-      </BrowserRouter>
+      <ThemeProvider theme={theme}>
+        <BrowserRouter>
+          <Switch>
+            {/** @TODO component 추가 */}
+            <Route exact path='/' component={Hello} />
+            <PrivateRoute path='/user' component={No} />
+          </Switch>
+        </BrowserRouter>
+      </ThemeProvider>
     </GoogleReCaptchaProvider>
   );
 };
