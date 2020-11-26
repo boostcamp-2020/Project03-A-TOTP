6d289da7d841165f3223741b33e05bdffb206059
JinGu <xortm854@naver.com>
2020-11-26 17:19:03 +0900

    fix : Alias 적용하여 경로 수정
    Alias를 설정하여 경로가 길어지지 않도록 바꿔줬습니다.

--- WEB/FE/src/index.tsx
+++ WEB/FE/src/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { App } from './components/App';
-import { GlobalStyle } from './style/globalStyle';
+import { App } from '@components/App';
+import { GlobalStyle } from '@styles/globalStyle';
 
 ReactDOM.render(
   <React.StrictMode>
