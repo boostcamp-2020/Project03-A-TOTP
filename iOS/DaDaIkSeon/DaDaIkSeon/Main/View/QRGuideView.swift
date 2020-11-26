91fd3f015a9e77bbe89098dbaf0551810a407f2f
jjm159 <wjdwoaud15@gmail.com>
2020-11-26 23:37:14 +0900

    [iOS] feat: QRGuideView에 NavigationView 추가
    - sheet로 띄워져도 네비게이션 타이틀과 백버튼이 보이도록 네비게이션 뷰로 감싸주었습니다.

--- iOS/DaDaIkSeon/DaDaIkSeon/Main/View/QRGuideView.swift
+++ iOS/DaDaIkSeon/DaDaIkSeon/Main/View/QRGuideView.swift
@@ -10,52 +10,55 @@ import CodeScanner
 
 struct QRGuideView: View {
     
+    // MARK: Property
     @State var showingScanner = false
     @Environment(\.presentationMode) var mode: Binding<PresentationMode>
     
+    // MARK: Body
     var body: some View {
-        VStack {
-            Spacer()
-            Button(action: {
-                showingScanner = true
-            }, label: {
-                HStack {
-                    Spacer()
-                    Image(systemName: "camera.fill")
-                    Text("QR 코드 스캔")
-                    Spacer()
-                }
-                .foregroundColor(.black)
-            })
-            .frame(width: 300, height: 50)
-            .background(Color(.systemGray6))
-            .cornerRadius(30)
-            
-        }
-        .navigationBarHidden(false)
-        .navigationBarTitle("QR 코드", displayMode: .inline)
-        .navigationBarBackButtonHidden(true)
-        .navigationBarItems(
-            leading: Button(action: {
-                self.mode.wrappedValue.dismiss()
-            }, label: {
-                Text("취소")
+        
+        NavigationView {
+            VStack {
+                Spacer()
+                Button(action: {
+                    showingScanner = true
+                }, label: {
+                    HStack {
+                        Image(systemName: "camera.fill")
+                        Text("QR 코드 스캔")
+                    }
                     .foregroundColor(.black)
-            })
-        )
-        .sheet(isPresented: $showingScanner) {
-            CodeScannerView(codeTypes: [.qr], simulatedData: "-") { result in
-                switch result {
-                case .success(let code):
-                    
-                    // 여기서 성공하면 새로운 화면 띄운다.
-                    // 키값도 넘겨준다.
-                    
-                    print(code)
-                case .failure(let error):
-                    print(error)
+                })
+                .frame(maxWidth: .infinity, maxHeight: 46)
+                .background(Color(.systemGray6))
+                .cornerRadius(15)
+            }
+            .padding(.horizontal, 40)
+            .navigationBarHidden(false)
+            .navigationBarTitle("QR 코드", displayMode: .inline)
+            .navigationBarBackButtonHidden(true)
+            .navigationBarItems(
+                leading: Button(action: {
+                    self.mode.wrappedValue.dismiss()
+                }, label: {
+                    Text("취소")
+                        .foregroundColor(.black)
+                })
+            )
+            .sheet(isPresented: $showingScanner) {
+                CodeScannerView(codeTypes: [.qr], simulatedData: "-") { result in
+                    switch result {
+                    case .success(let code):
+                        
+                        // 여기서 성공하면 새로운 화면 띄운다.
+                        // 키값도 넘겨준다.
+                        
+                        print(code)
+                    case .failure(let error):
+                        print(error)
+                    }
+                    self.showingScanner = false
                 }
-                self.showingScanner = false
             }
         }
         
@@ -63,6 +66,7 @@ struct QRGuideView: View {
     
 }
 
+// MARK: Preview
 struct QRGuideView_Previews: PreviewProvider {
     static var previews: some View {
         QRGuideView()
---
+++ iOS/DaDaIkSeon/DaDaIkSeon/Main/View/TokenEditView.swift
@@ -0,0 +1,20 @@
+//
+//  TokenEditView.swift
+//  DaDaIkSeon
+//
+//  Created by 정재명 on 2020/11/26.
+//
+
+import SwiftUI
+
+struct TokenEditView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct TokenEditView_Previews: PreviewProvider {
+    static var previews: some View {
+        TokenEditView()
+    }
+}
