1f1934525bb0a39e2745115a82c870041da1d2b1
jjm159 <wjdwoaud15@gmail.com>
2020-11-27 02:40:10 +0900

    [iOS] TokenCellViewModel에 로직 추가

--- iOS/DaDaIkSeon/DaDaIkSeon/Main/ViewModel/TokenViewModel.swift
+++ iOS/DaDaIkSeon/DaDaIkSeon/Main/ViewModel/TokenViewModel.swift
@@ -6,6 +6,8 @@
 //
 
 import Foundation
+import Combine
+import CryptoKit
 
 final class TokenViewModel: ObservableObject {
     
@@ -13,10 +15,119 @@ final class TokenViewModel: ObservableObject {
     
     @Published var token: Token
     
+    var key: String {
+        guard let key = token.key else {
+            return ""
+        }
+        return key
+    }
+    
+    @Published var tokenName = "토큰의이름은두줄두줄두줄두줄두줄두줄두줄두줄두줄"
+    
+    @Published var timeString = "1"
+    @Published var timeAmount = 0.0
+    @Published var password = ""
+    
+    let totalTime = 30.0
+    let timerInterval = 0.01
+    
+    var subscriptions = Set<AnyCancellable>()
+    
+    let timer: Publishers.Autoconnect<Timer.TimerPublisher>
+    
+    var lastSecond: Int = 1
+    
+    let dateFormmater = DateFormatter()
+    var todaySartTime: Date? {
+        let today = dateFormmater.string(from: Date())
+        return dateFormmater.date(from: today)
+    }
+    
+    
     // MARK: init
     
     init(token: Token) {
+        
         self.token = token
+        
+        dateFormmater.locale = Locale(identifier: "ko_KR")
+        dateFormmater.dateFormat = "yyyy-MM-dd"
+        
+        timer = Timer.publish(every: timerInterval, on: .main, in: .common)
+            .autoconnect()
+        
+        password = makePassword(key: key)
+        
+        timeAmount
+            = -Double(todaySartTime?.timeIntervalSinceNow ?? 0)
+            .truncatingRemainder(dividingBy: totalTime) + 1
+        
+        timer
+            .map({ (output) in
+                return output.timeIntervalSince(self.todaySartTime ?? Date())
+            })
+            .map({ (timeInterval) in
+                return Int(timeInterval) % Int(self.totalTime)
+            })
+            .sink { [weak self] (seconds) in
+                guard let weakSelf = self else { return }
+                if weakSelf.lastSecond != seconds {
+                    weakSelf.timeString = "\(seconds + 1)"
+                    if seconds == 0 {
+                        weakSelf.password
+                            = weakSelf.makePassword(key: weakSelf.key)
+                        weakSelf.timeAmount = 0
+                    }
+                    weakSelf.lastSecond = seconds
+                }
+                weakSelf.timeAmount += weakSelf.timerInterval
+            }
+            .store(in: &subscriptions)
+    }
+    
+    // MARK: Action
+    
+    func editButtonDidTab() {
+        print("EditButtonDidTab")
+    }
+    
+    func copyButtonDidTab() {
+        print("CopyButtonDidTab")
+    }
+    
+    // MARK: Logic
+    
+    func makePassword(key: String) -> String {
+        
+        let period = TimeInterval(totalTime)
+        let digits = 6
+        
+        // 키 값
+        guard let secret = Data(base64Encoded: key) else {
+            return "000000"
+        }
+        
+        // 현재 시간
+        var counter = UInt64(Date().timeIntervalSince1970 / period).bigEndian
+        let counterData = withUnsafeBytes(of: &counter) { Array($0) }
+        
+        // HMAC 알고리즘 연산
+        let hash = HMAC<Insecure.SHA1>.authenticationCode(
+            for: counterData, using: SymmetricKey(data: secret))
+        
+        // 추출
+        var truncatedHash = hash.withUnsafeBytes { ptr -> UInt32 in
+            let offset = ptr[hash.byteCount - 1] & 0x0f
+            guard let baseAddress = ptr.baseAddress else { return 0 }
+            let truncatedHashPtr = baseAddress + Int(offset)
+            return truncatedHashPtr.bindMemory(to: UInt32.self, capacity: 1).pointee
+        }
+        
+        truncatedHash = UInt32(bigEndian: truncatedHash)
+        truncatedHash = truncatedHash & 0x7FFF_FFFF
+        truncatedHash = truncatedHash & UInt32(pow(10, Float(digits)))
+        
+        return "\(String(format: "%0*u", digits, truncatedHash))"
     }
     
 }
