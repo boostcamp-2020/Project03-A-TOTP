3c2f6cadd8b82693f03fecaf28cb3baa55056f4b
eojin <eozinee@gmail.com>
2020-11-26 16:35:55 +0900

    [iOS] feat: TokenViewModel -> class로 변경

--- iOS/DaDaIkSeon/DaDaIkSeon/Main/ViewModel/TokenViewModel.swift
+++ iOS/DaDaIkSeon/DaDaIkSeon/Main/ViewModel/TokenViewModel.swift
@@ -7,48 +7,12 @@
 
 import Foundation
 
-struct TokenViewModel: Hashable {
+class TokenViewModel: ObservableObject {
     
-    var token: Token
+    @Published var token: Token
     
-    var id: UUID {
-        return UUID()
-    }
-    
-    var key: String {
-        guard let key = token.key else { return "" }
-        return key
-    }
-    
-    var tokenName: String {
-        guard let tokenName = token.tokenName else { return "" }
-        return tokenName
-    }
-    
-    var color: String {
-        guard let color = token.color else { return "" }
-        return color
-    }
-    
-    var icon: String {
-        guard let icon = token.icon else { return "" }
-        return icon
-    }
-    
-    static func == (lhs: TokenViewModel, rhs: TokenViewModel) -> Bool {
-        return lhs.id == rhs.id
-            && lhs.key == rhs.key
-            && lhs.tokenName == rhs.tokenName
-            && lhs.color == rhs.color
-            && lhs.icon == rhs.icon
-    }
-    
-    func hash(into hasher: inout Hasher) {
-        hasher.combine(id)
-        hasher.combine(key)
-        hasher.combine(tokenName)
-        hasher.combine(color)
-        hasher.combine(icon)
+    init(token: Token) {
+        self.token = token
     }
     
 }
