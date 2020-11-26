c7e2d5208e007e4cc3fb957d95f5a09efd8f141d
JinGu <xortm854@naver.com>
2020-11-26 17:37:32 +0900

    feat : Page 설정
    회원가입 페이지에 필요한 Layout설정을 위해 Page 모듈을 생성하였습니다.

---
+++ WEB/FE/src/pages/SignUpPage.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import SignUpComponent from '../components/SignUpComponent/SignUpComponent';
+
+const SignUpPage = () => {
+  return (
+    <div>
+      <SignUpComponent />
+    </div>
+  );
+};
+
+export default SignUpPage;
