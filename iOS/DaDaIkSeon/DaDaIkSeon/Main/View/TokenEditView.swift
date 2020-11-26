fb053ab650ea78d2e2def14f1adfd5321859381e
jjm159 <wjdwoaud15@gmail.com>
2020-11-26 23:39:03 +0900

    [iOS] feat: TokenEditView 추가

--- iOS/DaDaIkSeon/DaDaIkSeon/Main/View/TokenEditView.swift
+++ iOS/DaDaIkSeon/DaDaIkSeon/Main/View/TokenEditView.swift
@@ -8,8 +8,13 @@
 import SwiftUI
 
 struct TokenEditView: View {
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        
+        VStack {
+            
+        }
+        
     }
 }
 
