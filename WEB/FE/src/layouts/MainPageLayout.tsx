4500646ff9592a9b9e0dc2eb6d0c50580ddfc3d5
dogyeong <shroad1802@naver.com>
2020-11-29 02:10:19 +0900

    [FE] feat: MainPageLayout 컴포넌트 추가
    - 헤더, 푸터, 메인으로 구성

--- WEB/FE/src/layouts/MainPageLayout.tsx
+++ WEB/FE/src/layouts/MainPageLayout.tsx
@@ -1,12 +1,34 @@
 import React from 'react';
 import styled from 'styled-components';
+import { Header } from '@components/common/Header';
+import { Footer } from '@components/common/Footer';
+
+const Wrapper = styled.div`
+  width: 100%;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+`;
+
+const Main = styled.main`
+  flex: 1;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
 
 interface MainPageLayoutProps {
   children: React.ReactNode;
 }
 
 const MainPageLayout: React.FC<MainPageLayoutProps> = ({ children }) => {
-  return <>{children}</>;
+  return (
+    <Wrapper>
+      <Header />
+      <Main>{children}</Main>
+      <Footer />
+    </Wrapper>
+  );
 };
 
 export { MainPageLayout };
