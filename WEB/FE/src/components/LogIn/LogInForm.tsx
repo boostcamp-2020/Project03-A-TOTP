fdf5e0c6df1e97d5255e2021c6c9eaf73a4d75f6
dogyeong <shroad1802@naver.com>
2020-11-29 13:19:52 +0900

    [FE] feat: LogInForm title, button 추가
    - 제목,버튼 추가

--- WEB/FE/src/components/LogIn/LogInForm.tsx
+++ WEB/FE/src/components/LogIn/LogInForm.tsx
@@ -2,15 +2,29 @@ import React from 'react';
 import styled from 'styled-components';
 import Button from '@components/common/Button';
 
-const Wrapper = styled.form``;
+const Form = styled.form`
+  width: 100%;
+  max-width: 440px;
+  margin: auto;
+  padding: 4rem 2rem;
+  border-radius: 8px;
+  box-shadow: #ddd 0 0 20px 0;
+`;
+
+const Title = styled.h1`
+  text-align: center;
+  font-size: ${({ theme }) => theme.fontSize.xl};
+  margin-bottom: 2rem;
+`;
 
 interface LogInFormProps {}
 
 const LogInForm: React.FC<LogInFormProps> = () => {
   return (
-    <Wrapper>
-      <Button />
-    </Wrapper>
+    <Form>
+      <Title>LogIn</Title>
+      <Button text='로그인' onClick={() => {}} />
+    </Form>
   );
 };
 
