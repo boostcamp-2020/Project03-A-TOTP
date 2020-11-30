782fd12780837be03d082e9cb54aa9ef13f0b8f8
JinGu <xortm854@naver.com>
2020-12-01 02:23:33 +0900

    fix : 에러나는 부분 수정
    Input을 이전에 스타일을 따로 분리해뒀지만 다시 컴포넌트안에 스타일을
    선언하기로 하였기 때문에 바뀌었습니다.

--- WEB/FE/src/components/QRCodeComponent/QRCodeComponent.tsx
+++ WEB/FE/src/components/QRCodeComponent/QRCodeComponent.tsx
@@ -2,9 +2,10 @@ import React from 'react';
 import QRCODE from 'qrcode.react';
 import { Modal } from '@components/common/Modal';
 import styled from 'styled-components';
-import { ButtonTag } from '@styles/style';
 import { Link } from 'react-router-dom';
 import { Buffer } from 'buffer';
+import CSS from 'csstype';
+import Button from '@components/common/Button';
 
 const Title = styled.div`
   font-size: ${({ theme }) => theme.fontSize.xl};
@@ -31,21 +32,20 @@ const RegisterText = styled.div`
   margin-left: 20px;
 `;
 
-const ConfirmButton = styled(ButtonTag)`
-  width: 20%;
-  float: right;
-  color: white;
-  font-size: ${({ theme }) => theme.fontSize.md};
-  font-weight: 600;
-  margin-top: 18%;
-`;
-
+const buttonStyle: CSS.Properties = {
+  width: '20%',
+  float: 'right',
+  color: 'white',
+  fontSize: '${({ theme }) => theme.fontSize.md}',
+  fontFamily: '600',
+  marginTop: '18%',
+};
 interface qrProps {
   url: string;
 }
 
 const QRCodeComponent = ({ url }: qrProps): JSX.Element => {
-  const qrcode = decodeURIComponent(Buffer.from(url, 'base64').toString('ascii'));
+  const qrcode = Buffer.from(decodeURIComponent(url), 'base64').toString('ascii');
   return (
     <Modal>
       <Title>Register QR Code</Title>
@@ -58,7 +58,7 @@ const QRCodeComponent = ({ url }: qrProps): JSX.Element => {
           <ContentText>2. A 버튼을 눌러주세요.</ContentText>
           <ContentText>3. 카메라 중앙에 QR Code를 위치시켜주세요.</ContentText>
           <Link to='/'>
-            <ConfirmButton>Done</ConfirmButton>
+            <Button text='Done' style={buttonStyle} />
           </Link>
         </RegisterText>
       </ContentWrapper>
