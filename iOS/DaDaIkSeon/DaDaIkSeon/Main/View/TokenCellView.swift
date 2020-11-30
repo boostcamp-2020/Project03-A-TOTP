de86f6fb76f2f197148f8c50a12fc05e309a497b
eojin <eozinee@gmail.com>
2020-11-30 15:58:29 +0900

    [iOS] feat: TokenCellViewModel 생성

--- iOS/DaDaIkSeon/DaDaIkSeon/Main/View/TokenCellView.swift
+++ iOS/DaDaIkSeon/DaDaIkSeon/Main/View/TokenCellView.swift
@@ -8,12 +8,22 @@
 import SwiftUI
 import CryptoKit
 
+struct TokenCellState {
+    var service: TokenServiceable
+    var token: Token
+    var isShownEditView: Bool
+    var password: String
+}
+
+enum TokenCellInput {
+    case showEditView
+}
+
 struct TokenCellView: View {
     
     // MARK: ViewModel
     
-    //@Binding var viewModel: TokenViewModel
-    @ObservedObject var viewModel: TokenViewModel
+    @ObservedObject var viewModel: TokenCellViewModel
     
     // MARK: Body
     
@@ -27,16 +37,18 @@ struct TokenCellView: View {
                 Spacer()
                 
                 Button(action: {
-                    viewModel.showEditView = true
+//                    viewModel.showEditView = true
+                    // 변경 트리거 불러오기
+                    
                 }, label: {
                     Image(systemName: "ellipsis.circle.fill")
                         .resizable()
                         .frame(width: 20, height: 20, alignment: .top)
                         .foregroundColor(.white)
                 })
-                .sheet(isPresented: $viewModel.showEditView) {
-                    TokenEditView()
-                }
+//                .sheet(isPresented: viewModel.state.isShownEditView) {
+//                    TokenEditView()
+//                }
             }
             .padding(.horizontal, 12)
             .frame(height: 50, alignment: .center) // 크기를 자동으로 하는 방법 고민
@@ -44,7 +56,7 @@ struct TokenCellView: View {
             Spacer()
             
             HStack {
-                Text(viewModel.token.tokenName ?? "")
+                Text(viewModel.state.token.tokenName ?? "")
                     .font(.system(size: 11))
                     .foregroundColor(.white)
                     .lineLimit(2)
@@ -53,9 +65,9 @@ struct TokenCellView: View {
             .padding(.horizontal, 12)
             
             HStack {
-                (Text(viewModel.password.prefix(3))
+                (Text(viewModel.state.password.prefix(3))
                     + Text(" ")
-                    + Text(viewModel.password.suffix(3)))
+                    + Text(viewModel.state.password.suffix(3)))
                     .foregroundColor(.white)
                     .font(.system(size: 18))
                     .fontWeight(.bold)
@@ -64,7 +76,7 @@ struct TokenCellView: View {
             }
             .padding([.horizontal, .bottom], 12)
         }
-        .background(viewModel.color.linearGradientColor())
+        .background(viewModel.state.token.color?.linearGradientColor() ?? LinearGradient.pink)
         .cornerRadius(15)
         .shadow(color: Color.shadow, radius: 6, x: 0, y: 3.0)
     }
--- iOS/DaDaIkSeon/DaDaIkSeon/Main/ViewModel/TokenCellViewModel.swift
+++ iOS/DaDaIkSeon/DaDaIkSeon/Main/ViewModel/TokenCellViewModel.swift
@@ -6,3 +6,53 @@
 //
 
 import Foundation
+import Combine
+
+final class TokenCellViewModel: ViewModel {
+    
+    // MARK: Property
+    
+    @Published var state: TokenCellState
+    
+    let totalTime = TOTPTimer.shared.totalTime
+    let timerInterval = TOTPTimer.shared.timerInterval
+    
+    var subscriptions = Set<AnyCancellable>()
+    var lastSecond: Int = 1
+    
+    init(service: TokenServiceable, token: Token) {
+        state = TokenCellState(service: service,
+                               token: token,
+                               isShownEditView: false,
+                               password: TOTPGenerator.generate(from: token.key ?? "") ?? "000000")
+        
+        TOTPTimer.shared.timer
+            .map({ (output) in
+                output.timeIntervalSince1970
+            })
+            .map({ (timeInterval) in
+                Int(timeInterval.truncatingRemainder(dividingBy: self.totalTime))
+            })
+            .sink { [weak self] (seconds) in
+                guard let weakSelf = self else { return }
+                if weakSelf.lastSecond != seconds {
+                    if seconds == 0 {
+                        weakSelf.state.password
+                            = TOTPGenerator.generate(from: token.key ?? "") ?? "000000"
+                    }
+                    weakSelf.lastSecond = seconds
+                }
+            }
+            .store(in: &subscriptions)
+    }
+    
+    // MARK: Methods
+    
+    func trigger(_ input: TokenCellInput) {
+        switch input {
+        case .showEditView:
+            state.isShownEditView = true
+        }
+    }
+    
+}
