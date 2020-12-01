0df2be3f2923249d170d3a17ccf113d57e364023
liendmch <mon823@naver.com>
2020-12-01 16:56:39 +0900

    [BE] add: Redis session store로 사용
    - 기본 session 설정

--- WEB/BE/app.js
+++ WEB/BE/app.js
@@ -2,14 +2,18 @@ require('module-alias/register');
 const createError = require('http-errors');
 const express = require('express');
 const cookieParser = require('cookie-parser');
+const session = require('express-session');
+const RedisStore = require('connect-redis')(session);
 const logger = require('morgan');
 const cors = require('cors');
 const debug = require('debug')('server:server');
 const authRouter = require('@routes/auth');
 const userRouter = require('@routes/user');
 const csrf = require('@middlewares/csrf');
-const sequelizeWEB = require('./models/sequelizeIOS').sequelize;
-const sequelizeIOS = require('./models/sequelizeWEB').sequelize;
+const infoRouter = require('@routes/info');
+const redis = require('@models/redis');
+const sequelizeWEB = require('@models/sequelizeIOS').sequelize;
+const sequelizeIOS = require('@models/sequelizeWEB').sequelize;
 
 require('dotenv').config();
 
@@ -30,8 +34,22 @@ app.use(cookieParser());
 app.use(dev ? cors() : cors(corsOptions));
 app.use(csrf.checkHeader);
 
+app.use(
+  session({
+    store: new RedisStore({
+      client: redis(),
+      prefix: 'session:',
+    }),
+    saveUninitialized: false,
+    resave: false,
+    secret: process.env.SESSIONKEY,
+    cookie: { maxAge: 10000 },
+  })
+);
+
 app.use('/api/auth', authRouter);
 app.use('/api/user', userRouter);
+app.use('/api/info', infoRouter);
 
 // handle 404
 app.use((req, res, next) => {
--- WEB/BE/models/redis/index.js
+++ WEB/BE/models/redis/index.js
@@ -1,18 +1,18 @@
-const dotenv = require('dotenv').config();
+require('dotenv').config();
 const redis = require('redis');
 
-const redisOption = {
-  host: process.env.DBHOST,
+const client = () => {
+  const redisOption = {
+    host: process.env.DBHOST,
+  };
+
+  const client = redis.createClient(redisOption);
+  client.on('error', (error) => {
+    console.log(error);
+  });
+
+  return client;
 };
 
-const client = redis.createClient(redisOption);
-
-client.on('error', (error) => {
-  console.log(error);
-});
-
-// client.get('test', function (err, reply) {
-//   console.log(reply);
-// });
-
 // session 작업 이후에 고려
+module.exports = client;
