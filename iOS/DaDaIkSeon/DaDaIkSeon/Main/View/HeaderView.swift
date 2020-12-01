e140580112fd32518aff2de2219dc8003e704036
jjm159 <wjdwoaud15@gmail.com>
2020-12-01 22:44:27 +0900

    [iOS] feat: HeaderView에 hideCheckBox, showCheckBox 추가 (+ 코드 정리)
    - var 이용하여 view 분리

--- iOS/DaDaIkSeon/DaDaIkSeon/Main/View/HeaderView.swift
+++ iOS/DaDaIkSeon/DaDaIkSeon/Main/View/HeaderView.swift
@@ -17,43 +17,67 @@ struct HeaderView: View {
     
     var body: some View {
         HStack {
-            Button(action: {
-                print("내 정보 버튼 Did tap")
-            }, label: {
-                if viewModel.state.checkBoxMode {
-                    
-                    let count = viewModel.state.selectedTokens
-                        .filter { ( key, _ ) in
-                        viewModel.state.selectedTokens[key] == true
-                    }.count
-                    Text("\(count)개 삭제")
-                } else {
-                    Image.person.resizable()
-                        .frame(width: 25)
-                }
-            })
-            .frame(height: 25)
-            .foregroundColor(.black)
-            .padding(.leading, 4)
-            
-            Spacer()
-            
-            Button(action: {
-                viewModel.state.checkBoxMode ?
-                    viewModel.trigger(.hideCheckBox)
-                    : viewModel.trigger(.showCheckBox)
-            }, label: {
-                if viewModel.state.checkBoxMode {
-                    Text("취소")
-                        .padding(.trailing, 4)
-                        .foregroundColor(.black)
-                } else {
-                    Text("선택")
-                        .padding(.trailing, 4)
-                        .foregroundColor(.black)
-                }
-            })
+            if viewModel.state.checkBoxMode {
+                deleteButton
+                    .frame(height: 25)
+                    .foregroundColor(.black)
+                    .padding(.leading, 4)
+                Spacer()
+                cancelButton
+            } else {
+                settingButton
+                    .frame(height: 25)
+                    .foregroundColor(.black)
+                    .padding(.leading, 4)
+                Spacer()
+                selectButton
+            }
         }.padding([.leading, .trailing], 16)
+    }
+    
+    // MARK: 삭제, 세팅 버튼
+    
+    var deleteButton: some View {
+        Button(action: {
+            viewModel.trigger(.deleteSelectedTokens)
+        }, label: {
+            let count = viewModel.state.selectedTokens
+                .filter { ( key, _ ) in
+                    viewModel.state.selectedTokens[key] == true
+                }.count
+            Text(count == 0 ? "" : "\(count)개 삭제")
+        })
+    }
+    
+    var settingButton: some View {
+        Button(action: {
+            viewModel.trigger(.startSetting)
+        }, label: {
+            Image.person.resizable()
+                .frame(width: 25)
+        })
+    }
+    
+    // MARK: 취소 선택 버튼
+    
+    var cancelButton: some View {
+        Button(action: {
+            viewModel.trigger(.hideCheckBox)
+        }, label: {
+            Text("취소")
+                .padding(.trailing, 4)
+                .foregroundColor(.black)
+        })
+    }
+    
+    var selectButton: some View {
+        Button(action: {
+            viewModel.trigger(.showCheckBox)
+        }, label: {
+            Text("선택")
+                .padding(.trailing, 4)
+                .foregroundColor(.black)
+        })
     }
     
 }
