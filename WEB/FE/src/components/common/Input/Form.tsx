26c211a9e6e6d9ad7907186ae94c155ee7ac0e35
JinGu <xortm854@naver.com>
2020-12-02 15:55:55 +0900

    feat : Input 태그를 감싸고 있는 레이아웃
    Input 태그를 깜싸고 오른쪽 버튼의 필요에 따라 보여줘야하기 때문에
    레이아웃을 만들어서 모든 Input들이 레이아웃을 사용할 수 있도록
    하였습니다.

---
+++ WEB/FE/src/components/common/Input/Form.tsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import styled from 'styled-components';
+
+export const Wrapper = styled.div`
+  display: flex;
+  justify-content: center;
+  border: 2px solid darkgray;
+  border-radius: 5px;
+  margin-top: 20px;
+  overflow: hidden;
+  position: relative;
+`;
+
+const Button = styled.button`
+  flex: 0.2;
+  height: 36px;
+  border: none;
+`;
+
+interface Props {
+  children: React.ReactNode;
+  buttonEvent?: (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => Promise<void>;
+}
+
+const Form = ({ children, buttonEvent }: Props): JSX.Element => {
+  return (
+    <Wrapper>
+      {children}
+      {buttonEvent ? <Button onClick={(e) => buttonEvent(e)}>verify</Button> : undefined}
+    </Wrapper>
+  );
+};
+
+Form.defaultProps = {
+  buttonEvent: undefined,
+};
+
+export default Form;
