09a164d247b808ff2894258a967eb5130c8149da
eojin <eozinee@gmail.com>
2020-12-02 12:10:40 +0900

    [iOS] feat: TokenEditViewModel 생성
    - addToken
    - changeColor
    - changeIcon

--- iOS/DaDaIkSeon/DaDaIkSeon/Main/ViewModel/TokenEditViewModel.swift
+++ iOS/DaDaIkSeon/DaDaIkSeon/Main/ViewModel/TokenEditViewModel.swift
@@ -6,3 +6,30 @@
 //
 
 import Foundation
+
+final class TokenEditViewModel: ViewModel {
+    
+    // MARK: Property
+    
+    @Published var state: TokenEditState
+    
+    init(service: TokenServiceable, token: Token?, qrCode: String?) {
+        state = TokenEditState(service: service,
+                               qrCode: qrCode,
+                               token: token)
+    }
+    
+    // MARK: Methods
+    
+    func trigger(_ input: TokenEditInput) {
+        switch input {
+        case .addToken(let token):
+            state.service.add(token: token)
+        case .changeColor(let color):
+            state.token?.color = color
+        case .changeIcon(let icon):
+            state.token?.icon = icon
+        }
+    }
+
+}
