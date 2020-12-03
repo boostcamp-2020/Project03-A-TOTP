c875b745d427a1535d75c391e9e21f69729a8b67
liendmch <mon823@naver.com>
2020-12-03 11:46:13 +0900

    [FE] add: findID page 연결
    - FindId page 라우터 연결

--- WEB/FE/src/components/App.tsx
+++ WEB/FE/src/components/App.tsx
@@ -26,6 +26,7 @@ const App: React.FC<AppProps> = () => {
         <Route exact path='/signup' component={Pages.SignUpPage} />
         <Route exact path='/login' component={Pages.LogInPage} />
         <Route exact path='/QRCode/:url' component={Pages.QRCodePage} />
+        <Route exact path='/findId' component={Pages.findIDPage} />
         <Route component={NotFound} />
       </Switch>
     </BrowserRouter>
