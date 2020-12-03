85e1984d62757cd80d710fbfad12dc9997f83f82
liendmch <mon823@naver.com>
2020-12-03 18:43:06 +0900

    [FE] add:  비밀번호 찾기 Component 완성
    - 기본적인 예외 처리 alter
      - 파람이 없는 경우
      - 비밀번호가 일치하지 않는 경우
      - 비밀번호 입력을 안한 경우
      - 실패 한경우

---
+++ WEB/FE/src/components/findPass/findPasswordComponent.tsx
@@ -0,0 +1,73 @@
+import React, { useEffect, useState } from 'react';
+import qs from 'qs';
+import { useInput } from '@hooks/useInput';
+import { PasswordInput } from '@components/common';
+import { Redirect, useHistory } from 'react-router-dom';
+import { message } from '@utils/message';
+import { changePass } from '@/api';
+import { AuthForm } from '../common/AuthForm';
+
+const FindPasswordComponent = ({ location }): JSX.Element => {
+  const { search } = location;
+  const [password, setPassword] = useInput('');
+  const [rePassword, setRePassword] = useInput('');
+  const [accord, setAccord] = useState(true);
+  const history = useHistory();
+
+  useEffect(() => {
+    if (password !== rePassword) setAccord(false);
+    else setAccord(true);
+  }, [password, rePassword]);
+  if (!search) {
+    return <Redirect to='/' />;
+  }
+
+  const userData = qs.parse(search, {
+    ignoreQueryPrefix: true,
+  });
+
+  const querStr: string = userData.user!.toString();
+
+  const onSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    if (!password || !rePassword) {
+      alert(message.SOMETHINGNOTINPUT);
+    } else if (!accord) {
+      alert(message.PASSWORDNOTACCORD);
+    } else {
+      changePass(querStr, password)
+        .then(() => {
+          history.push('/login');
+        })
+        .catch((err: any) => alert(err.response?.data?.mesaage || err));
+    }
+  };
+
+  return (
+    <AuthForm
+      title='비밀번호 변경'
+      action='api/user/find-pass'
+      onSubmit={onSubmit}
+      submitButtonText='변경하기'
+    >
+      <PasswordInput
+        value={password}
+        type='password'
+        showExplanation
+        placeholder='Password'
+        onChange={setPassword}
+      />
+      <PasswordInput
+        value={rePassword}
+        type='password'
+        showExplanation
+        representWarning
+        state={accord}
+        placeholder='Password'
+        onChange={setRePassword}
+      />
+    </AuthForm>
+  );
+};
+
+export { FindPasswordComponent };
