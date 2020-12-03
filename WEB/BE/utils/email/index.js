c1d64bdafd4790df67aa7f7a77da5c06fc3a3b6b
liendmch <mon823@naver.com>
2020-12-03 16:08:42 +0900

    [BE] add: 비밀번호 찾기 이메일 전송 함수 구현
    - request 에서 axios 로 변경 사용
    - 비밀번호 찾기 sender 구현

--- WEB/BE/utils/email/index.js
+++ WEB/BE/utils/email/index.js
@@ -1,11 +1,11 @@
-const request = require('request');
 const { encryptWithSHA256, encryptWithAES256 } = require('@utils/crypto');
+const axios = require('axios');
 
 const ACCESSKEY = process.env.EMAILACCESSKEY;
 const SECRETKEY = process.env.EMAILSECRETKEY;
 
 const emailSender = {
-  SignUpAuthentication(address, name, idx) {
+  async SignUpAuthentication(address, name, idx) {
     const time = Date.now();
     const result = encryptWithAES256({ Text: `${address} ${time + 7200000} ${idx}` });
     const resultURL = `https://dadaikseon.com/confirm-email?user=${encodeURIComponent(result)}`;
@@ -17,15 +17,14 @@ const emailSender = {
     const option = makeOption(2324, address, name, parameters);
 
     try {
-      request.post(option, (err, httpResponse, body) => {});
+      // request.post(option, (err, httpResponse, body) => {});
+      await axios(option);
     } catch (e) {
-      /**
-       * @TODO 에러 처리
-       */
+      throw new Error(e);
     }
   },
 
-  sendId(address, name, id) {
+  async sendId(address, name, id) {
     const parameters = {
       userName: name,
       userId: id,
@@ -33,12 +32,27 @@ const emailSender = {
     const option = makeOption(2368, address, name, parameters);
 
     try {
-      request.post(option, (err, httpResponse, body) => {});
+      // request.post(option, (err, httpResponse, body) => {});
+      await axios(option);
     } catch (e) {
-      console.log(e);
-      /**
-       * @TODO 에러 처리
-       */
+      throw new Error(e);
+    }
+  },
+
+  async sendPassword(user, url) {
+    const { email, name } = user;
+    const parameters = {
+      userName: name,
+      URL: url,
+    };
+
+    const option = makeOption(2373, email, name, parameters);
+
+    try {
+      // request.post(option, (err, httpResponse, body) => {});
+      await axios(option);
+    } catch (e) {
+      throw new Error(e);
     }
   },
 };
@@ -47,14 +61,14 @@ const makeOption = (templateSid, address, name, parameters) => {
   const time = Date.now();
   const payload = `POST /api/v1/mails\n${time}\n${ACCESSKEY}`;
   const option = {
-    uri: process.env.EMAILURL,
+    url: process.env.EMAILURL,
     method: 'POST',
     headers: {
       'x-ncp-apigw-timestamp': time,
       'x-ncp-iam-access-key': ACCESSKEY,
       'x-ncp-apigw-signature-v2': encryptWithSHA256(SECRETKEY, payload),
     },
-    body: {
+    data: {
       templateSid,
       individual: true,
       advertising: false,
@@ -67,7 +81,6 @@ const makeOption = (templateSid, address, name, parameters) => {
         },
       ],
     },
-    json: true,
   };
   return option;
 };
