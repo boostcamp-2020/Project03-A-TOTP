8e45fbb66722e46ec2e4a755d433f3725af3863e
JinGu <xortm854@naver.com>
2020-12-04 00:21:40 +0900

    feat : TOTP Modal을 위한 컴포넌트 구현 -#113
    아이디, 이름, 생년월일이 일치하는 사용자가 있다면 TOTP 모달창을 띄워서
    6자리를 입력할 수 있도록해야한다. 때문에 이를 위해 인증이 성공하면
    AuthToken을 받아오고 이와 동시에 Modal 창을 띄워 TOTP 6자리를
    입력받으며, 입력된 6자리는 AuthToken과 함꼐 서버로 전송된다.

---
+++ WEB/FE/src/components/FindPassword/FindPasswordContainer.tsx
@@ -0,0 +1,65 @@
+import React, { useState } from 'react';
+import { FindPasswordForm } from '@/components/FindPassword/FindPasswordForm';
+import { TOTPModal } from '@components/TOTPModal/TOTPModal';
+import { findPasswordWithOTP } from '@api/index';
+import { useGoogleReCaptcha } from 'react-google-recaptcha-v3';
+import { useHistory } from 'react-router-dom';
+
+const TOTP_LEN = 6;
+
+interface FindPasswordContainerProps {}
+
+const FindPasswordContainer = ({}: FindPasswordContainerProps): JSX.Element => {
+  const history = useHistory();
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const { executeRecaptcha } = useGoogleReCaptcha();
+  const [TOTP, setTOTP] = useState('');
+  const [authToken, setAuthToken] = useState('');
+  const [hasTOTPModalError, setHasTOTPModalError] = useState(false);
+  const [modalDisabled, setModalDisabled] = useState(false);
+  const [errorMsg, setErrorMsg] = useState('');
+
+  const onSuccessAuthToken = (authToken: string) => {
+    setAuthToken(authToken);
+    setIsModalOpen(true);
+  };
+  const onCloseModal = () => {
+    setTOTP('');
+    setIsModalOpen(false);
+  };
+  const onErrorWithOTP = (errMsg: string) => {
+    setHasTOTPModalError(true);
+    setErrorMsg(errMsg);
+  };
+  const onSubmitOTP = () => {
+    setHasTOTPModalError(false);
+    if (TOTP.length < TOTP_LEN) {
+      onErrorWithOTP('전부 입력해 주세요~');
+      return;
+    }
+    setModalDisabled(true);
+    executeRecaptcha('findPasswordWithOTP')
+      .then((reCaptchaToken: string) => findPasswordWithOTP({ authToken, totp: TOTP, reCaptchaToken }))
+      .then(() => history.replace('/'))
+      .catch((err: any) => onErrorWithOTP(err.response?.data?.message || err.message))
+      .finally(() => setModalDisabled(false));
+  };
+
+  return (
+    <>
+      <FindPasswordForm onSuccess={onSuccessAuthToken} />
+      <TOTPModal
+        isOpen={isModalOpen}
+        TOTP={TOTP}
+        onChange={setTOTP}
+        onSubmit={onSubmitOTP}
+        onClose={onCloseModal}
+        hasErrored={hasTOTPModalError}
+        disabled={modalDisabled}
+        errorMsg={errorMsg}
+      />
+    </>
+  );
+};
+
+export { FindPasswordContainer };
