76c24357fc7ed56ac036440641fb9211f35ab384
jjm159 <wjdwoaud15@gmail.com>
2020-12-06 02:10:36 +0900

    [iOS] test: Test 파일 정리

---
+++ iOS/DaDaIkSeon/DaDaIkSeonTests/TOTPGenerator/DDISTOTPGeneratorTests.swift
@@ -0,0 +1,15 @@
+//
+//  DDISTOTPGeneratorTests.swift
+//  DaDaIkSeonTests
+//
+//  Created by 정재명 on 2020/12/06.
+//
+
+import XCTest
+import SwiftOTP
+
+class DDISTOTPGenerationTests: XCTestCase {
+    
+    
+    
+}
