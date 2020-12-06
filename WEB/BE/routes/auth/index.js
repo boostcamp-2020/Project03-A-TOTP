1ee3d137fe616b92aec7e88127c45895fe165ae5
dogyeong <shroad1802@naver.com>
2020-12-06 13:58:23 +0900

    [BE] refactor: 에러 핸들링 리팩토링
    - 라우터 코드에 catchErrors 추가해서 예외처리하도록 수정
    - controller, service 코드에서 try/catch, next 넘겨주는 코드 제거

--- WEB/BE/routes/auth/index.js
+++ WEB/BE/routes/auth/index.js
@@ -5,16 +5,17 @@ const authController = require('@controllers/auth');
 const { validator } = require('@middlewares/validator');
 const reCAPTCHA = require('@middlewares/reCAPTCHA');
 const { verifyJWT } = require('@middlewares/verifyJWT');
+const { catchErrors } = require('@utils/util');
 
-router.post('/dup-id', authController.dupId);
+router.post('/dup-id', catchErrors(authController.dupId));
 router
   .route('/')
-  .post(reCAPTCHA.verify, validator(['id', 'password']), authController.logIn)
-  .put(verifyJWT.verifyTOTP, authController.logInSuccess);
+  .post(reCAPTCHA.verify, validator(['id', 'password']), catchErrors(authController.logIn))
+  .put(verifyJWT.verifyTOTP, catchErrors(authController.logInSuccess));
 router
   .route('/password/email')
-  .post(reCAPTCHA.verify, validator(['id', 'name', 'birth']), authController.sendPasswordToken)
-  .put(reCAPTCHA.verify, verifyJWT.verifyTOTP, authController.sendPasswordEmail)
-  .patch(validator(['password']), authController.changePassword);
+  .post(reCAPTCHA.verify, validator(['id', 'name', 'birth']), catchErrors(authController.sendPasswordToken))
+  .put(reCAPTCHA.verify, verifyJWT.verifyTOTP, catchErrors(authController.sendPasswordEmail))
+  .patch(validator(['password']), catchErrors(authController.changePassword));
 
 module.exports = router;
