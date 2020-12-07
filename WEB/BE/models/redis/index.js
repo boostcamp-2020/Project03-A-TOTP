94078ce7d307116029248263743f1d4166742034
liendmch <mon823@naver.com>
2020-12-07 16:25:44 +0900

    [BE] add: redis 이벤트 수신 확인
    - redis 에서 만료된 key 를 수신확인

--- WEB/BE/models/redis/index.js
+++ WEB/BE/models/redis/index.js
@@ -7,10 +7,19 @@ const client = () => {
   };
 
   const client = redis.createClient(redisOption);
+  client.config('SET', 'notify-keyspace-events', 'Ex');
+
+  const subscriber = redis.createClient(redisOption);
   client.on('error', (error) => {
     console.log(error);
   });
 
+  subscriber.on('pmessage', function (pattern, channel, message) {
+    /** @TODO log 변경 */
+    console.log(`pattern : ${pattern} channel: ${channel} message : ${message}`);
+  });
+  subscriber.psubscribe('__key*__:*');
+  client.setex('string key', 10, 'string val', redis.print);
   return client;
 };
 
