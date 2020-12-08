c7281135fadc87f4e352d0dde5304932356d5b78
jjm159 <wjdwoaud15@gmail.com>
2020-12-08 23:26:08 +0900

    [iOS] chore: isDetailLink 삭제

--- iOS/DaDaIkSeon/DaDaIkSeon/Scene/Main/View/QRGuideView.swift
+++ iOS/DaDaIkSeon/DaDaIkSeon/Scene/Main/View/QRGuideView.swift
@@ -58,7 +58,7 @@ struct QRGuideView: View {
                                   qrCode: TOTPGenerator.extractKey(from: qrCodeURL))),
                 isActive: $isShownEditView
             )
-            .isDetailLink(false)
+
         }
         .padding(.horizontal, 40)
         .navigationBarHidden(false)
