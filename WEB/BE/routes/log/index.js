1819e33b063447be5cdc51f64360cafbd170d06a
JinGu <xortm854@naver.com>
2020-12-08 23:29:33 +0900

    [BE]feat: Session 확인 및 로그 라우터 구현 -#207
    사용자의 요청을 받아줄 라우터를 구현해야하기 때문에 요청을 받아줄
    라우터를 구현하였습니다. 또한 로그인 이후 부터의 요청은 Session값과
    csrfToken을 확인하여 요청을 확인하고 있습니다.

---
+++ WEB/BE/routes/log/index.js
@@ -0,0 +1,12 @@
+const express = require('express');
+
+const router = express.Router();
+const logController = require('@controllers/log');
+const { sessionAuthentication } = require('@middlewares');
+const { catchErrors } = require('@utils/util');
+
+router.use(sessionAuthentication.sessionCheck);
+
+router.get('/:num', catchErrors(logController.get));
+
+module.exports = router;
