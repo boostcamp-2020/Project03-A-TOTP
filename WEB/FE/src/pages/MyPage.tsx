6e2096deb5ef6348532bc9fae1d409161e85998f
JinGu <xortm854@naver.com>
2020-12-10 06:51:51 +0900

    [FE]refactor: 메인페이지에서 로그인 리다이렉트
    메인페이지에서 로그인이 되어있지 않다면 로그인페이지로 리다이렉트하게
    해줬습니다.

--- WEB/FE/src/pages/MyPage.tsx
+++ WEB/FE/src/pages/MyPage.tsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import { MainPageLayout } from '@layouts/MainPageLayout';
 import { MyPageContainer } from '@components/MyPage/MyPageContainer';
+import { Redirect } from 'react-router-dom';
 
 interface MyPageProps {}
 
 function MyPage({}: MyPageProps): JSX.Element {
+  if (!localStorage.user) {
+    return <Redirect to='/login' />;
+  }
   return (
     <MainPageLayout>
       <MyPageContainer />
