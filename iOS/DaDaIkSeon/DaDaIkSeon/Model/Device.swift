dcb2662cd8d8e214266fa655fae7be8edadacf24
eojin <eozinee@gmail.com>
2020-12-10 17:01:59 +0900

    [iOS] feat: User, Token, Device model 수정

--- iOS/DaDaIkSeon/DaDaIkSeon/Model/Device.swift
+++ iOS/DaDaIkSeon/DaDaIkSeon/Model/Device.swift
@@ -12,17 +12,21 @@ struct Device: Codable, Equatable {
     var name: String?
     var udid: String?
     var modelName: String?
+    var backup: Bool?
+    var lastUpdate: String?
     
     static func == (lhs: Device, rhs: Device) -> Bool {
         return lhs.name == rhs.name
             && lhs.udid == rhs.udid
             && lhs.modelName == rhs.modelName
+            && lhs.backup == rhs.backup
+            && lhs.lastUpdate == rhs.lastUpdate
     }
     
     static func dummy() -> [Device] {
         [
-            Device(name: "jjm", udid: "a", modelName: "아이폰"),
-            Device(name: "kjg", udid: "b", modelName: "갤럭시")
+            Device(name: "jjm", udid: "a", modelName: "아이폰", backup: false, lastUpdate: "2020-11-21"),
+            Device(name: "kjg", udid: "b", modelName: "갤럭시", backup: false, lastUpdate: "2020-11-23")
         ]
     }
     
