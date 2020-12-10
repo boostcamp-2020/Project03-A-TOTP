f515f2418e6f8920ae42776b26916f4493bcfbcd
dogyeong <shroad1802@naver.com>
2020-12-10 15:49:42 +0900

    [FE] feat: 로그인 상태에 따라 헤더 정보 수정
    - localStorage의 상태에 따라 헤더 다르게 보이도록 수정
    - Header를 Header, Nav, Dropdown으로 분리
    - Dropdown 추가

--- WEB/FE/src/components/PrivateRoute/PrivateRoute.tsx
+++ WEB/FE/src/components/PrivateRoute/PrivateRoute.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import { Redirect, Route, RouteProps } from 'react-router-dom';
+import storageHandler from '@utils/localStorage';
 
 interface PrivateRouteProps extends RouteProps {}
 
 const PrivateRoute: React.FC<PrivateRouteProps> = (props) => {
-  const isAuthenticated = localStorage.user;
+  const isAuthenticated = storageHandler.get();
 
   return isAuthenticated ? (
     <Route {...props} />
--- WEB/FE/src/layouts/MainPageLayout.tsx
+++ WEB/FE/src/layouts/MainPageLayout.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import styled from 'styled-components';
-import { Header } from '@components/common/Header';
+import { Header } from '@/components/common/Header/Header';
 import { Footer } from '@components/common/Footer';
 
 const Wrapper = styled.div`
