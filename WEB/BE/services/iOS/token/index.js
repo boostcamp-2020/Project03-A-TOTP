ba7ea4083ac5e7a40609ef99e2a2e1aaef26717c
liendmch <mon823@naver.com>
2020-12-10 17:23:14 +0900

    [BE] add:  service
    - get
    - post
    - patch
    - delete
    구현

---
+++ WEB/BE/services/iOS/token/index.js
@@ -0,0 +1,28 @@
+const { getTokenList, updateToken, delToken } = require('@/controllers/iOS/token');
+const { tokens: Token } = require('@models/sequelizeIOS');
+
+const toeknService = {
+  async addTokens(params) {
+    console.log(params);
+    await Token.bulkCreate(params, { ignoreDuplicates: true });
+    return true;
+  },
+
+  async getTokens(params) {
+    const result = await Token.findAll({ where: params });
+    return result;
+  },
+
+  async updateToken(params, id) {
+    console.log(params, id);
+    const result = await Token.update(params, { where: { id } });
+    return result;
+  },
+
+  async delToken({ id }) {
+    const result = await Token.destroy({ where: { id } });
+    return result;
+  },
+};
+
+module.exports = toeknService;
