eec443d8e2f2895ee8bce842d4123a3108740ff3
jjm159 <wjdwoaud15@gmail.com>
2020-12-12 01:59:59 +0900

    [iOS] fix: 생체 인식 수정 완료!😁
    - 스위치를 통해 화면 구분
    - active 상태가 되면 핀코드가 있는지 확인, 있다면 생체인증 시도
    - 불가하거나 실패하면 핀넘버 입력 화면으로 전환
    - 인증 완료시 메인화면 진입
    - 단지 메인 뷰의 뷰모델을 StateObject로 바꿨을 뿐인데 해결!

--- iOS/DaDaIkSeon/DaDaIkSeon/Common/DaDaIkSeonApp.swift
+++ iOS/DaDaIkSeon/DaDaIkSeon/Common/DaDaIkSeonApp.swift
@@ -10,68 +10,68 @@ import SwiftUI
 @main
 struct DaDaIkSeonApp: App {
     
-    @State var main = false
-    @State var pincode = false
-    
-    var pincodeManager = PincodeManager()
-    var bioAuth = BiometricIDAuth()
-    
+    @State var root: DaDaIkSeonAppRootViews = .none
     @Environment(\.scenePhase) var scenePhase
-
+    
     var body: some Scene {
         
-        let storageManager = StorageManager()
-        let service = TokenService(storageManager)
+        let service = TokenService(StorageManager())
         
         WindowGroup {
-            // 아래에서 상태 만들고 여기서 조건문으로 분기
-            
-            // 각 뷰의 상태값을 만듬
-            // 조건문에 따라 다른 뷰 생성
-            
-            if main {
+            switch root {
+            case .main:
                 MainView(service: service).environmentObject(NavigationFlowObject())
-            } else if pincode {
-                if let pincode = pincodeManager.loadPincode() {
+            case .localAuth:
+                if let pincode = PincodeManager().loadPincode() {
                     PinCodeView(mode: .auth(pincode), completion: { _ in
-                        self.pincode = false
-                        main = true
+                        root = .main
                     })
                 }
+            case .networkAuth:
+                EmptyView()
+            case .none:
+                EmptyView()
             }
-            // 대체 화면이 있어야 하나? - 배경을 달아놓을까?
         }
         .onChange(of: scenePhase, perform: { newScenePhrase in
             switch newScenePhrase {
+            case .inactive:
+                DispatchQueue.main.async { root = .none }
             case .active:
-                guard !main && !pincode else { return } // 하나라도 true면 실행 못하게
-                // jwt를 핀코드보다 먼저 검사해야할지 고민
-                if let pincode = pincodeManager.loadPincode() {
-                    bioAuth.authenticateUser { result in
-                        if result == nil { // 생체인증 성공
-                            print("성공")
-                            DispatchQueue.main.async {
-                                main = true
-                            }
-                        } else {
-                            if "You pressed cancel." == result
-                                || "You pressed password." == result {
-                                print("cancel", "실패, 핀넘버로")
-                                DispatchQueue.main.async {
-                                    self.pincode = true
-                                }
-                            }
-                        }
-                    }
-                } else {
-                    DispatchQueue.main.async {
-                        self.main = true // pincode 없으면 앱 보호 안하겠다는 것
-                    }
-                }
+                // JWT가 있을 때 하는 로직 - JWT가 없으면 인증 화면으로!
+                // 로컬 인증 함수는 JWT가 있으면 실행되면 안된다.
+                localAuthenticate()
             default: break
             }
         })
-        
     }
     
+}
+
+extension DaDaIkSeonApp {
+    func localAuthenticate() {
+        switch root {
+        case .none:
+            if nil != PincodeManager().loadPincode() {
+                BiometricIDAuth().authenticateUser { result in
+                    if result == nil { // 생체인증 성공
+                        DispatchQueue.main.async { root = .main }
+                    } else {            // 생체인증 실패
+                        DispatchQueue.main.async { root = .localAuth}
+                    }
+                }
+            } else { // pincode 없으면 앱 보호 안하겠다는 것
+                DispatchQueue.main.async { root = .main }
+            }
+        default:
+            return
+        }
+    }
+}
+
+enum DaDaIkSeonAppRootViews {
+    case main
+    case localAuth
+    case networkAuth
+    case none
 }
