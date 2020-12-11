22c483cb64bd62928da4023dbd006c74d368a8c7
JinGu <xortm854@naver.com>
2020-12-11 09:02:51 +0900

    [BE]fix: 자릿수 문제 해결
    substring함수를 사용하였기 때문에 두번째 인자로 원하는 개수를 입력하도록
    바꿨습니다.

--- WEB/BE/utils/random/index.js
+++ WEB/BE/utils/random/index.js
@@ -1,9 +1,6 @@
 const makeRandom = (num = 6, length = undefined) => {
   return Array.from(Array(num).keys()).reduce(
-    (prev, now) =>
-      (prev += Math.random()
-        .toString(36)
-        .substr(2, length ? length + 2 : undefined)),
+    (prev, now) => (prev += Math.random().toString(36).substr(2, length)),
     ''
   );
 };
