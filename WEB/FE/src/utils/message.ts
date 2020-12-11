276fac628e30ff4696bfc342511e7757935fe644
JinGu <xortm854@naver.com>
2020-12-11 18:13:05 +0900

    [FE]refactor: 메세지 추가
    결과창을 보여주기 위해 메세지를 추가하였습니다.

--- WEB/FE/src/utils/message.ts
+++ WEB/FE/src/utils/message.ts
@@ -12,4 +12,5 @@ export enum message {
   SIGNINSUCCESS = '로그인에 성공하셨습니다.',
   FINDPASSWORDSUCCESS = '패스워드 변경을 위한 주소를 이메일로 전송하였습니다.',
   EMAILSECRETKEYSUCCESS = '이메일로 QR Code를 재발급 받을 수 있는 주소를 전송하였습니다.',
+  CHANGEPASSWORDSUCCESS = '비밀번호가 성공적으로 변경되었습니다.',
 }
