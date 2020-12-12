01ff7eb26fbeb70e663183e285e15deb5a95bed4
dogyeong <shroad1802@naver.com>
2020-12-12 23:23:31 +0900

    [FE] feat: react-helmet 적용
    - title, meta 태그 추가

--- WEB/FE/src/index.tsx
+++ WEB/FE/src/index.tsx
@@ -6,15 +6,22 @@ import { GoogleReCaptchaProvider } from 'react-google-recaptcha-v3';
 import { ThemeProvider } from 'styled-components';
 import { GlobalStyle } from '@styles/GlobalStyle';
 import { theme } from '@styles/theme';
+import { Helmet } from 'react-helmet';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <GoogleReCaptchaProvider reCaptchaKey='6LcsnewZAAAAAOHzbLjkR4CvWRBNdibrcmtHd8SD' language='ko'>
-      <ThemeProvider theme={theme}>
-        <GlobalStyle />
-        <App />
-      </ThemeProvider>
-    </GoogleReCaptchaProvider>
-  </React.StrictMode>,
+  <>
+    <Helmet>
+      <title>다다익선</title>
+      <meta name='description' content='더 안전한 TOTP 서비스. 다다익선에서 시작하세요.' />
+    </Helmet>
+    <React.StrictMode>
+      <GoogleReCaptchaProvider reCaptchaKey='6LcsnewZAAAAAOHzbLjkR4CvWRBNdibrcmtHd8SD' language='ko'>
+        <ThemeProvider theme={theme}>
+          <GlobalStyle />
+          <App />
+        </ThemeProvider>
+      </GoogleReCaptchaProvider>
+    </React.StrictMode>
+  </>,
   document.getElementById('app'),
 );
