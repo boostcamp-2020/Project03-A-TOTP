2adfbcadf209d5b773c7f64c183a86f9a855ddc7
JinGu <xortm854@naver.com>
2020-12-13 08:10:18 +0900

    [BE]doc: Log Router Swagger -#274
    로그 라우터 스웨거 작성

--- WEB/BE/routes/web/log/index.js
+++ WEB/BE/routes/web/log/index.js
@@ -8,6 +8,35 @@ const authController = require('@/controllers/web/auth');
 
 router.use(sessionAuthentication.sessionCheck);
 
+/**
+ * @swagger
+ * /log/:num:
+ *  put:
+ *    tags:
+ *      - log
+ *    summary: 선택페이지 로그가져오기
+ *    description: 페이지네이션을 적용한 개수만큼 로그가져오기
+ *    produces:
+ *    - "application/json"
+ *    parameters:
+ *    - name: num
+ *      in: params
+ *      description: 로그페이지 숫자
+ *      type: string
+ *      required: true
+ *    responses:
+ *      200:
+ *        description: 로그 가져오기 성공
+ *        schema:
+ *          type: object
+ *          properties:
+ *            result:
+ *              type: Object Array
+ *      401:
+ *        description: csrf 에러 또는 세션 에러
+ *      500:
+ *        description: 기타 에러
+ */
 router.get('/:num', catchErrors(logController.get));
 
 /**
