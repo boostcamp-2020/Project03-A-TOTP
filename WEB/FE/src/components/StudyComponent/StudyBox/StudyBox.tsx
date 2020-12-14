4511f6ec737b31c0cc13c530ee136aad50ee2508
JinGu <xortm854@naver.com>
2020-12-15 05:05:27 +0900

    [FE]feat: 학습하기를 위한 컴포넌트 구현-#244
    학습하기를 진행하기위해서 필요한 컴포넌트들을 구성합니다. Absolute한
    컴포넌트들의 위치들을 정해주어 직접 위치를 맞춰줬습니다. 또한 상태값으로
    배열의 값이 들어있다면 드래그 컴포넌트를 보여줍니다.

---
+++ WEB/FE/src/components/StudyComponent/StudyBox/StudyBox.tsx
@@ -0,0 +1,48 @@
+import React from 'react';
+import { Drag } from '@/components/StudyComponent/DragAndDrop/Drag';
+import styled from 'styled-components';
+import { initialArrayData } from '@/utils/initialData';
+import { DropTarget } from '../DragAndDrop/DropTarget';
+
+const DropContainer = styled.div<{ styles: any; isList: boolean }>`
+  position: ${(props) => (props.isList ? 'relative' : 'absolute')};
+  left: ${(props) => props.styles.left}px;
+  top: ${(props) => props.styles.top}px;
+  width: ${(props) => props.styles.width}px;
+  height: ${(props) => props.styles.height}px;
+  overflow: visible;
+  justify-content: ${(props) => (props.isList ? 'none' : 'center')};
+  display: flex;
+  flex-wrap: wrap;
+  margin: auto;
+  border: ${(props) => (props.isList ? '2px solid black' : 'none')};
+`;
+
+const DragImg = styled.img<{ styles: any }>`
+  width: ${(props) => props.styles.width - 5}px;
+  height: ${(props) => props.styles.height - 5}px;
+`;
+
+interface StudyBoxProps {
+  value: string;
+  list: { [name: string]: string }[];
+  onDrop: (e: React.DragEvent<HTMLDivElement>, name: string) => void;
+}
+
+const StudyBox = ({ value, list, onDrop }: StudyBoxProps): JSX.Element => {
+  return (
+    <DropTarget onDrop={(e) => onDrop(e, value)}>
+      <DropContainer styles={initialArrayData[value]} isList={value === 'list'}>
+        {list.map((item: any): any => {
+          return (
+            <Drag dataItem={`${value}:${item.name}`}>
+              <DragImg styles={initialArrayData[item.name]} src={item.svg} />
+            </Drag>
+          );
+        })}
+      </DropContainer>
+    </DropTarget>
+  );
+};
+
+export { StudyBox };
