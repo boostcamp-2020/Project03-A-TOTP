0e8d259ede98c7b7cdadefe226295c06686994bb
dogyeong <shroad1802@naver.com>
2020-12-15 17:15:00 +0900

    [FE] feat: 푸터에 깃헙 링크 추가
    - 깃헙 로고 버튼 추가
    - 누르면 깃헙 저장소로 이동

--- WEB/FE/src/components/Footer/Footer.tsx
+++ WEB/FE/src/components/Footer/Footer.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import styled from 'styled-components';
+import githubImage from '@static/github.svg';
 
 const Wrapper = styled.footer`
   height: 108px;
@@ -13,6 +14,14 @@ const Inner = styled.div`
   display: flex;
   justify-content: center;
   align-items: center;
+
+  .github-logo {
+    img {
+      width: 22px;
+      vertical-align: bottom;
+      margin-left: 1rem;
+    }
+  }
 `;
 
 interface FooterProps {}
@@ -21,7 +30,16 @@ const Footer: React.FC<FooterProps> = () => {
   return (
     <Wrapper>
       <Inner>
-        <span>&copy; {new Date().getFullYear()} Boostcamp. All right reserved</span>
+        <span>
+          &copy; {new Date().getFullYear()} Boostcamp. All right reserved{' '}
+          <a
+            className='github-logo'
+            href='https://github.com/boostcamp-2020/Project03-A-TOTP'
+            target='_blank'
+          >
+            <img src={githubImage} alt='github logo' />
+          </a>
+        </span>
       </Inner>
     </Wrapper>
   );
