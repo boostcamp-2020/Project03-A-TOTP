5244dbf254bfad7d1a67c8bdb97d1cf844510c20
JinGu <xortm854@naver.com>
2020-12-15 19:11:47 +0900

    [BE]fix: 코드 수정
    CSRF Token 확인하는 코드 수정

--- WEB/BE/middlewares/sessionAuthentication.js
+++ WEB/BE/middlewares/sessionAuthentication.js
@@ -4,7 +4,7 @@ const sessionAuthentication = {
   sessionCheck(req, res, next) {
     const { CSRF_TOKEN } = req.session;
     const { csrfToken } = req.query;
-    if (!req.session.user && CSRF_TOKEN !== csrfToken) {
+    if (!req.session.user || CSRF_TOKEN !== csrfToken) {
       res.clearCookie('csrfToken');
       return next(createError(401, '로그아웃되어 권한이 없습니다.'));
     }
@@ -15,7 +15,6 @@ const sessionAuthentication = {
   },
 
   sessionLogout(req, res, next) {
-    // key 유무 확인을 먼저 해야하나?????
     req.session.destroy((err) => {
       if (err) {
         throw err;
