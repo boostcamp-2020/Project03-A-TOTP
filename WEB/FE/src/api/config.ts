2f7387b2781a5d8639ac1d636f74538dff7bbece
JinGu <xortm854@naver.com>
2020-12-15 22:48:39 +0900

    [FE]feat: Response Interceptor 설정 -#343
    Response Interceptor를 설정하여 Response일 때 401 에러가 발생하면 세션이
    없음을 의미하기 때문에 Local Storage를 삭제하고, 페이지를 reload하게
    하였습니다.

--- WEB/FE/src/api/config.ts
+++ WEB/FE/src/api/config.ts
@@ -1,4 +1,6 @@
 import axios, { AxiosRequestConfig } from 'axios';
+import storageHandler from '@utils/localStorage';
+import { message } from '../utils/message';
 
 const Axios = axios.create({
   headers: { 'X-CSRF': 'X-CSRF' },
@@ -12,5 +14,17 @@ Axios.interceptors.request.use((value: AxiosRequestConfig) => {
   };
   return config;
 });
+
+Axios.interceptors.response.use(
+  (response) => response,
+  (error) => {
+    if (error.response.status === 401) {
+      storageHandler.clear();
+      alert(error.response.data.message);
+      window.location.reload();
+    }
+    return Promise.reject(error);
+  },
+);
 
 export default Axios;
