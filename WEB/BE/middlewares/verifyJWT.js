8b6017cbcbcec667fb14bc846514c948c1415a39
liendmch <mon823@naver.com>
2020-12-15 12:09:34 +0900

    [BE] feat: 기존 정보와 OTP 정보 비교
    - Error 메시지 전달

--- WEB/BE/middlewares/verifyJWT.js
+++ WEB/BE/middlewares/verifyJWT.js
@@ -10,6 +10,11 @@ const verifyJWT = {
       const { id, action } = JWT.verify(authToken, process.env.ENCRYPTIONKEY);
       req.body.id = id;
       req.body.action = action;
+
+      const lastOtp = await authService.getOTPById({ id });
+      if (String(lastOtp) === digits)
+        next(createError(400, '한번 사용된 OTP입니다 다음 OTP 정보를 이용하세요'));
+
       const secretKey = (await authService.getAuthById({ id })).secret_key;
       const result = totp.verifyDigits(secretKey, digits);
       if (!result) next(createError(400, 'TOTP 6자리가 틀렸습니다.'));
